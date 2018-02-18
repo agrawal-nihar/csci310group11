b8e500715b3417f24a15e901be955d182956d176
Shing Tak Ryan To <shingtat@usc.edu>
2018-02-17 16:03:38 -0800

    finished initial page

---
+++ frontend/js/index.js
@@ -0,0 +1,10 @@
+function handleDisable(){
+  var button = document.getElementById("build");
+  var inputText = document.getElementById("text_input");
+  if(inputText.value == 0 || inputText.value == "" || inputText.value == null){
+    button.disabled = true;
+  }
+  else{
+    button.disabled = false;
+  }
+}
