e48848244909c7854e5a4d1df165e6caf4ec9f18
Allen <allenyhu@usc.edu>
2018-02-19 17:58:16 -0800

    test

--- src/main/java/csci310group11/Implementation/CollageGenerator.java
+++ src/main/java/csci310group11/Implementation/CollageGenerator.java
@@ -30,4 +30,26 @@ public class CollageGenerator {
 		
 		return filename;
 	}
+
+	public BufferedImage addBorderToImages(BufferedImage image) {
+		int width = image.getWidth();
+		int height = image.getHeight();
+		
+		BufferedImage borderedImage = new BufferedImage(width + 6, height + 6, image.getType());
+		
+		
+		
+		return borderedImage;
+	}
+	
+	/**
+	 * Resize: 1/20th of collage
+	 */
+	
+	/*
+	 * make images 1/2*totalarea and lay in center of each quad
+	 * rest of images must be 2/13 * totalarea
+	 * lay so that they all make it onto the page
+	 * 
+	 */
 }
