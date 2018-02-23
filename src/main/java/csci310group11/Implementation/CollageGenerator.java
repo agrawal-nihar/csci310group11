2895d86f9bad0d8458026ab77bebbd06bc92c90e
Peter Chen <chenhaoy@usc.edu>
2018-02-22 23:05:43 -0800

    some initialization
    1. Collage class
    2. Constants class
    3. fix downloadCollage in CollageGenerator

--- src/main/java/csci310group11/Implementation/CollageGenerator.java
+++ src/main/java/csci310group11/Implementation/CollageGenerator.java
@@ -1,6 +1,6 @@
 package csci310group11.Implementation;
 
-import javq.io.*;
+import java.io.*;
 import java.awt.image.*;
 import javax.imageio.*;
 
@@ -9,11 +9,11 @@ public class CollageGenerator {
 	//download collage to the server
 	public void downloadCollage(Collage collage) {
 		try {
-			BufferedImage image = collage.collage; //getter?
-			String fileName = collage.topic;
-			fileName+= “_” + collage.id  + “.png”;    //if collage has a unique id?
-			File outputFile = new File(topic);
-			ImageIO.write(image, “png”, outputFile);
+			BufferedImage image = collage.getCollageImage(); //getter?
+			String fileName = collage.getTopic();
+			fileName += "_" + collage.id  + ".png";    //if collage has a unique id?
+			File outputFile = new File(fileName);
+			ImageIO.write(image, "png", outputFile);
 		}
 		catch(Exception e) {
 			e.printStackTrace();
