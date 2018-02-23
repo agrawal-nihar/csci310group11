a4145904b7abf9801c73c1662a340e6da8bf6400
Allen Huang <allenhuang@allens-mbp.attlocal.net>
2018-02-22 22:00:46 -0800

    finish addBorderToImages()

--- src/main/java/csci310group11/Implementation/CollageGenerator.java
+++ src/main/java/csci310group11/Implementation/CollageGenerator.java
@@ -1,5 +1,6 @@
 package csci310group11.Implementation;
 
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 
 import java.io.File;
@@ -9,21 +10,28 @@ import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
 
+import javafx.scene.layout.BorderImage;
+
 public class CollageGenerator {
 	private ArrayList<String> imageUrls; //change to <BufferedImage> if necessary
 	private ArrayList<BufferedImage> images;
 	private ArrayList<BufferedImage> borderedImages;
+	private BufferedImage collage;
 
 	public CollageGenerator(ArrayList<String> urls) {
 		this.imageUrls = new ArrayList<String>();
 		this.images = new ArrayList<BufferedImage>();
 		this.borderedImages = new ArrayList<BufferedImage>();
+		this.collage = new BufferedImage();
 
 		this.imageUrls = urls;
 		for(int i=0; i < imageUrls.size(); i++) {
 			URL url = new URL(imageUrls.get(i));
-			this.borderedImages.add(ImageIO.read(url));
+			this.images.add(ImageIO.read(url));
 		}
+		this.resizeImages();
+		this.addBorderToImages();
+		this.compileCollage();
 	}
 
 	private String downloadCollage(Collage collage) {
@@ -49,23 +57,48 @@ public class CollageGenerator {
 		return filename;
 	}
 
-	
+	/**
+	 * Getter for the collage image
+	 * 
+	 * @return the BufferedImage collage
+	 */
+	public BufferedImage getCollage() {
+		return this.collage;
+	}
 
-	public void addBorderToImages(BufferedImage image) {
-		int width = image.getWidth();
-		int height = image.getHeight();
-		
-		BufferedImage borderedImage = new BufferedImage(width + 6, height + 6, image.getType());
-		//paint image onto new borderedImage
+	/**
+	 * Responsible for adding a 3px white border to each image to be added to the collage.
+	 * 
+	 * Creates a new BufferedImage that is 6px taller and wider than the original BufferedImage in images.
+	 * 
+	 * Sets thes the graphics of the larger BufferedImage to white. Paints the original image onto the 
+	 * new BufferedImage to create a 3px "border". Adds the bordred BuffereImage to this.borderedImages.
+	 */
+	private void addBorderToImages() {
+		for(int i=0; i < image.size(); i++) {
+			BufferedImage image = image.get(i);
+			int width = image.getWidth();
+			int height = image.getHeight();
+			
+			//Create image with enough space for 3px border
+			BufferedImage borderedImage = new BufferedImage(width + 6, height + 6, image.getType());
 
+			//Setting larger image to all white
+			Graphics2D graphics = borderedImage.getGraphics();
+			graphics.setPaint(new Color(0,0,0));
+			graphics.fillRect(0, 0, borderedImage.getWidth(), borderedImage.getHeight());
 
-		this.borderedImages.add(borderedImage);
+			//Paint original image onto new borderedImage	
+			graphics.drawImage(image, 2, 2, null);
+			this.borderedImages.add(borderedImage);
+			
+		}
 	}
 	
 	/**
 	 * Resize: 1/20th of collage
 	 */
-	public void resizeImage(BufferedImage image) {
+	private void resizeImages() {
 
 	}
 
@@ -73,4 +106,7 @@ public class CollageGenerator {
 	 * Cover corners, lay inside and see what happens
 	 * 
 	 */
+	private void compileCollage() {
+
+	}
 }
