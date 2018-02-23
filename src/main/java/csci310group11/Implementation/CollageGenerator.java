d37c3b03e2da2e9f427195d2fcde655d012eb679
Allen Huang <allenhuang@allens-mbp.attlocal.net>
2018-02-22 21:16:17 -0800

    constructor

--- src/main/java/csci310group11/Implementation/CollageGenerator.java
+++ src/main/java/csci310group11/Implementation/CollageGenerator.java
@@ -1,50 +1,34 @@
 package csci310group11.Implementation;
 
-<<<<<<< HEAD
 import java.awt.image.BufferedImage;
 
-
 public class CollageGenerator {
-	public BufferedImage addBorderToImages(BufferedImage image) {
+	private ArrayList<String> imageUrls; //change to <BufferedImage> if necessary
+	private ArrayList<BufferedImage> borderedImages;
+
+	public CollageGenerator(ArrayList<String> urls) {
+		this.images = new ArrayList<BufferedImage> borderedImages;
+		this.imageUrls = new ArrayList<String>();
+		this.imageUrls = urls;
+	}
+
+	public void addBorderToImages(BufferedImage image) {
 		int width = image.getWidth();
 		int height = image.getHeight();
 		
 		BufferedImage borderedImage = new BufferedImage(width + 6, height + 6, image.getType());
-		
-		
-		
-		return borderedImage;
+		this.borderedImages.add(borderedImage);
 	}
 	
 	/**
 	 * Resize: 1/20th of collage
 	 */
-	
+	public void resizeImage(BufferedImage image) {
+
+	}
+
 	/*
-	 * make images 1/2*totalarea and lay in center of each quad
-	 * rest of images must be 2/13 * totalarea
-	 * lay so that they all make it onto the page
+	 * Cover corners, lay inside and see what happens
 	 * 
 	 */
-=======
-import javq.io.*;
-import java.awt.image.*;
-import javax.imageio.*;
-
-public class CollageGenerator {
-	
-	//download collage to the server
-	public void downloadCollage(Collage collage) {
-		try {
-			BufferedImage image = collage.collage; //getter?
-			String fileName = collage.topic;
-			fileName+= “_” + collage.id  + “.png”;    //if collage has a unique id?
-			File outputFile = new File(topic);
-			ImageIO.write(image, “png”, outputFile);
-		}
-		catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
->>>>>>> d72df59eab3ae3171239cf4f8f1757970575c851
 }
