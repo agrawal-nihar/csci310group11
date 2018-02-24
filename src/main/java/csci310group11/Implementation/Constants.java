1141d1104f33466236a583587757a03c791f12e6
Allen Huang <allenyhu@usc.edu>
2018-02-24 15:22:52 -0800

    border width constant

--- src/main/java/csci310group11/Implementation/Constants.java
+++ src/main/java/csci310group11/Implementation/Constants.java
@@ -4,7 +4,7 @@ import java.awt.Color;
 
 public class Constants {
 	static double AVERAGE_SIZE = 0.05;
-	static double BORDER_WIDTH = 0.0;
+	static int BORDER_WIDTH = 100;
 	static Color BORDER_COLOR = Color.WHITE;
 	static double ROTATION_MIN_DEGREE = 0.0;
 	static double ROTATION_MAX_DEGREE = 180.0;
