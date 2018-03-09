6ab5c4af10a95a8ea0dce6ae377bc0fa8a5bf031
Haoyang Chen <chenhaoy@usc.edu>
2018-03-09 03:18:37 -0800

    Revert "Submit whitebox"

--- src/main/java/csci310group11/Implementation/Utility.java
+++ src/main/java/csci310group11/Implementation/Utility.java
@@ -4,7 +4,6 @@ import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -16,8 +15,6 @@ import javax.imageio.ImageIO;
 
 public class Utility {
 	
-	public static String dummyImageFile = "/home/student/Desktop/dummy.txt";
-	
 	//helper method to clear all logging files
 	public static void clearAllLoggingFiles(ArrayList<String> list) throws FileNotFoundException {
 		for (int i = 0; i < list.size(); i++) {
@@ -28,7 +25,6 @@ public class Utility {
 	
 	//helper method to clear log file 
 	public static void clearFile(String filename) throws FileNotFoundException {
-		
 		PrintWriter writer = new PrintWriter(filename);
 		writer.print("");
 		writer.close();
@@ -58,7 +54,7 @@ public class Utility {
 	}
 	
 	//helper method to read from file
-	public static String readFromFile(String filename) {
+	public void readFromFile(String filename) {
 		System.out.println(filename + " PRINTOUT is: ");
 		String lineRead = "";
 		String fileContents = "";
@@ -78,47 +74,25 @@ public class Utility {
 			e.printStackTrace();   
 		} 
 	  System.out.println(fileContents);
-	  
-	  return fileContents;
 	}
 	
 	public static void printImageByteSize(BufferedImage image, String destination) throws IOException {
-		clearFile(dummyImageFile);
-		
-//		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-//		ImageIO.write(image, "png", outputStream);
-//		outputStream.flush();
-//		byte[] imageInByte = outputStream.toByteArray();
-
-		//write to disk
-		ImageIO.write(image, "png", new File(dummyImageFile));
-		
-		//read from disk
-		File imageFile = new File(dummyImageFile);
-		System.out.println("IMage file length: " + imageFile.length());
-		Utility.writeToFile(String.valueOf(imageFile.length()), "/home/student/Desktop/imagefilelengthsize.txt");
-		//
-		
-		BufferedImage imageRetrieved = ImageIO.read(new File(dummyImageFile));
 		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-		ImageIO.write(imageRetrieved, "png", outputStream);
+		ImageIO.write(image, "png", outputStream);
 		outputStream.flush();
-//		byte[] imageInByte = outputStream.toByteArray();
-		
-		writeToFile(String.valueOf(outputStream.size()), destination);
-		outputStream.close();
+		byte[] imageInByte = outputStream.toByteArray();
 
 		
+		writeToFile(String.valueOf("12345"), destination);
+		writeToFile(String.valueOf(imageInByte.length), destination);
 		
 		outputStream.close();
 	}
 	
 	public static void printAllCollagesBase64String(ArrayList<String> data, String allCollagesFile) throws IOException {
 		clearFile(allCollagesFile);
-		String toAppend = "data:image/png;base64, ";
 		for (int i = 0; i < data.size(); i++) {
-			String toWrite = toAppend + data.get(i);
-			writeToFile(toWrite, allCollagesFile);
+			writeToFile(data.get(i), allCollagesFile);
 		}
 	}
 
